e278968ef423c7a1cb0171ce8e63bfe4e481837f
Sousa <114963739+ESousa97@users.noreply.github.com>
2025-01-09 22:05:09 -0300

    ATT

--- assets/js/fetchApis.js
+++ assets/js/fetchApis.js
@@ -1,9 +1,9 @@
-let { API_URL } = process.env;
+const API_URL = process.env.API_URL || 'https://instabyte-back-imersao-807011187654.southamerica-east1.run.app/posts';
 
 // Função para buscar os dados do endpoint
 export default async function fetchImages() {
   try {
-    const response = await fetch(process.env.API_URL); // Usando a URL importada
+    const response = await fetch(API_URL); // Usando a URL importada
     const data = await response.json();
     return data;
   } catch (error) {
